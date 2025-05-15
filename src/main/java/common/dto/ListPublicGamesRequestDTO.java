ab3f30cac031c6957e434acc0ea2a49f14de5c21
AmrGenidy <amrogenidy@gmail.com>
2025-05-16 00:48:45 +0300

    Emptying the main branch

--- src/main/java/common/dto/ListPublicGamesRequestDTO.java
+++
@@ -1,8 +0,0 @@
-package common.dto;
-
-import java.io.Serializable;
-
-// No fields needed, just a marker DTO
-public class ListPublicGamesRequestDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
-}
